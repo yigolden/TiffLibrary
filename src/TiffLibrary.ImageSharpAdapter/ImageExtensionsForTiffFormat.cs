3d5aac2ff13246fe14fb28f764638b80ecdc320c
yigolden <jinyi@live.cn>
2020-07-22 14:14:44 +0800

    Add more extensions methods of SaveAsTiff[Async] for ImageSharp image.

--- src/TiffLibrary.ImageSharpAdapter/ImageExtensionsForTiffFormat.cs
+++ src/TiffLibrary.ImageSharpAdapter/ImageExtensionsForTiffFormat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using SixLabors.ImageSharp.Advanced;
 using TiffLibrary.ImageSharpAdapter;
 
@@ -10,6 +11,86 @@ namespace SixLabors.ImageSharp
     /// </summary>
     public static class ImageExtensionsForTiffFormat
     {
+        /// <summary>
+        /// Saves the image to the given stream with the TIFF format.
+        /// </summary>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="path">The file path to save the image to.</param>
+        public static void SaveAsTiff(this Image source, string path)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (path is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            source.SaveAsTiff(path, null);
+        }
+
+        /// <summary>
+        /// Saves the image to the given stream with the TIFF format.
+        /// </summary>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="path">The file path to save the image to.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static Task SaveAsTiffAsync(this Image source, string path)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (path is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return SaveAsTiffAsync(source, path, null);
+        }
+
+        /// <summary>
+        /// Saves the image to the given stream with the TIFF format.
+        /// </summary>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="path">The file path to save the image to.</param>
+        /// <param name="encoder">The encoder to save the image with.</param>
+        public static void SaveAsTiff(this Image source, string path, TiffEncoder? encoder)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (path is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            source.Save(path, encoder ?? source.GetConfiguration().ImageFormatsManager.FindEncoder(TiffFormat.Instance));
+        }
+
+        /// <summary>
+        /// Saves the image to the given stream with the TIFF format.
+        /// </summary>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="path">The file path to save the image to.</param>
+        /// <param name="encoder">The encoder to save the image with.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static Task SaveAsTiffAsync(this Image source, string path, TiffEncoder? encoder)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (path is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return source.SaveAsync(path, encoder ?? source.GetConfiguration().ImageFormatsManager.FindEncoder(TiffFormat.Instance));
+        }
+
         /// <summary>
         /// Saves the image to the given stream with the TIFF format.
         /// </summary>
@@ -49,6 +130,49 @@ namespace SixLabors.ImageSharp
             }
 
             source.Save(stream, encoder ?? source.GetConfiguration().ImageFormatsManager.FindEncoder(TiffFormat.Instance));
+        }
+
+        /// <summary>
+        /// Saves the image to the given stream with the TIFF format.
+        /// </summary>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="stream">The stream to save the image to.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static Task SaveAsTiffAsync(this Image source, Stream stream)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (stream is null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            return SaveAsTiffAsync(source, stream, null);
+        }
+
+        /// <summary>
+        /// Saves the image to the given stream with the TIFF format.
+        /// </summary>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="stream">The stream to save the image to.</param>
+        /// <param name="encoder">The encoder to save the image with.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static Task SaveAsTiffAsync(this Image source, Stream stream, TiffEncoder? encoder)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (stream is null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+
+            return source.SaveAsync(stream, encoder ?? source.GetConfiguration().ImageFormatsManager.FindEncoder(TiffFormat.Instance));
+
         }
     }
 }
